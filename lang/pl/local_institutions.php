b8fbb772b873d778d2f17f54daf25e46fc5c31a0
Bruno Sampaio <Bruno@c-3po.home>
2013-07-13 11:56:59 +0100

    v1.0.3
    Polish translation added

---
+++ lang/pl/local_institutions.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * Strings for component 'local_institutions', language 'pl', branch 'MOODLE_20_STABLE'
+ *
+ * @package   local_institutions
+ * @copyright 2013 Bruno Sampaio
+ * translater: Jaroslaw Maciejewski 'Nitro' <jmaciejewski@webnitro.eu>
+ */
+
+$string['pluginname'] = 'Instytucje';
+
+$string['institution'] = 'Instytucja';
+$string['institutions'] = 'Instytucje';
+
+// Actions
+$string['add-institution'] = 'Dodaj nową instytucję';
+$string['edit-institution'] = 'Edytuj instytucję';
+$string['delete-institution'] = 'Usuń instytucję';
+$string['no-institution'] = 'Instytucja nie istnieje';
+
+// Fields
+$string['field-fullname'] = 'Pełna nazwa';
+$string['field-shortname'] = 'Krótka nazwa';
+$string['field-url'] = 'Strona URL';
+$string['field-icon'] = 'Logo URL';
+$string['field-address'] = 'Adres';
+$string['field-phone'] = 'Telefon';
+$string['field-description'] = 'Opis';
+
+// Success
+$string['success-delete'] = 'Instytut został pomyślnie skasowany.';
+
+// Warnings
+$string['warning-empty'] = 'Nie ma tutaj żadnych dostępnych instytutów.';
+
+// Errors
+$string['error-database-table'] = 'Tabela instytucji w bazie danych nie została odnaleziona w twoim systemie.';
+$string['error-delete'] = 'Instytut nie może zostać skasowany.';
+$string['error-iframe'] = 'Twoja przeglądarka nie obsługuje ramek HTML. Proszę użyć innej przeglądarki lub zaktualizować istniejącą.';
+$string['error-institution'] = 'Błąd Instytutu';
+$string['error-noid'] = 'ID Instytutu musi być określone.';
+$string['error-permissions'] = 'Nie masz uprawnień do wykonania tej akcji.';
